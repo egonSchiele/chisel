5711cb91e24d6eb8400e12146df297b0ebcb5531
Aditya Bhargava <adit@adit.io>
2023-04-16 17:46:20 -0500

    integration test for login

---
+++ cypress/e2e/Login.cy.ts
@@ -0,0 +1,19 @@
+import settings from "../../settings.js";
+
+describe("login", () => {
+  it("works", () => {
+    cy.visit("http://localhost:80/login.html");
+    cy.contains("h2", "Sign in");
+    cy.get("input[name=email]").type(settings.testuser.email);
+
+    // {enter} causes the form to submit
+    cy.get("input[name=password]").type(`${settings.testuser.password}{enter}`);
+
+    // our auth cookie should be present
+    cy.getCookie("userid").should("exist");
+    cy.getCookie("token").should("exist");
+
+    // UI should reflect this user being logged in
+    cy.contains("h3", "Books");
+  });
+});
