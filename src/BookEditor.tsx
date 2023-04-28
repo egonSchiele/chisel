2e05104b8f41ae41baf0e017d4f22b5c4c0020eb
Aditya Bhargava <adit@adit.io>
2023-04-28 18:40:28 -0500

    Show chapters on book editor page, special book editor page for compost heap.

--- src/BookEditor.tsx
+++ src/BookEditor.tsx
@@ -7,6 +7,8 @@ import TextArea from "./components/TextArea";
 import { Character } from "./Types";
 import Button from "./components/Button";
 import Input from "./components/Input";
+import { getChapterText } from "./utils";
+import { Link, useNavigate } from "react-router-dom";
 
 function Character({
   character,
@@ -63,11 +65,46 @@ function Character({
   );
 }
 
+function Chapter({ chapter, bookid }) {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="flex flex-col my-sm bg-gray-200 dark:bg-gray-700 rounded-md p-sm cursor-pointer"
+      onClick={() => navigate(`/book/${bookid}/chapter/${chapter.chapterid}`)}
+    >
+      <h3 className="text-xl font-semibold">{chapter.title}</h3>
+      <p className="text-gray-800 dark:text-gray-300">
+        {getChapterText(chapter).slice(0, 500)}
+      </p>
+    </div>
+  );
+}
+
+function CompostBook() {
+  const book = useSelector(getSelectedBook);
+  return (
+    <div className="mx-auto px-sm lg:px-md mt-0 h-full w-full">
+      <h1 className="text-2xl mb-sm tracking-wide font-semibold text-darkest dark:text-lightest">
+        Compost Heap
+      </h1>
+      <div className="grid gap-sm grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
+        {book.chapters &&
+          book.chapters.map((chapter, i) => (
+            <Chapter key={i} chapter={chapter} bookid={book.bookid} />
+          ))}
+      </div>
+    </div>
+  );
+}
+
 export default function BookEditor() {
   const book = useSelector(getSelectedBook);
   const dispatch = useDispatch();
   if (!book) {
     return <div>loading</div>;
+  }
+  if (book.tag === "compost") {
+    return <CompostBook />;
   }
   return (
     <div className="flex h-screen overflow-scroll w-full">
@@ -91,21 +128,21 @@ export default function BookEditor() {
           inputClassName="typography border-0 bg-editor text-editortext dark:bg-dmeditor dark:text-dmeditortext resize-none"
           rows={8}
         />
-        {book.tag !== "compost" && (
-          <div className="text-xl font-semibold mt-md mb-xs">
-            <span>Characters</span>
-            <Button
-              onClick={() => {
-                dispatch(librarySlice.actions.addCharacter());
-              }}
-              rounded={true}
-              className="ml-xs"
-              size="small"
-            >
-              Add Character
-            </Button>
-          </div>
-        )}
+
+        <div className="text-xl font-semibold mt-md mb-xs">
+          <span>Characters</span>
+          <Button
+            onClick={() => {
+              dispatch(librarySlice.actions.addCharacter());
+            }}
+            rounded={true}
+            className="ml-xs"
+            size="small"
+          >
+            Add Character
+          </Button>
+        </div>
+
         <div className="grid gap-sm grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
           {book.characters &&
             book.characters.map((character, i) => (
@@ -142,6 +179,17 @@ export default function BookEditor() {
               />
             ))}
         </div>
+
+        <div className="text-xl font-semibold mt-md mb-xs">
+          <span>Chapters</span>
+        </div>
+        <div className="grid gap-sm grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
+          {book.chapters &&
+            book.chapters.map((chapter, i) => (
+              <Chapter key={i} chapter={chapter} bookid={book.bookid} />
+            ))}
+        </div>
+
         {/* bottom padding */}
         <div className="h-24" />
       </div>
