c46a7cb80dd1745ff4ebb7aa64ddebe15e1e2efd
Aditya Bhargava <adit@adit.io>
2023-04-29 09:50:08 -0500

    raw diffs

--- src/DiffViewer.tsx
+++ src/DiffViewer.tsx
@@ -1,13 +1,30 @@
 import React from "react";
-
+import * as JsDiff from "diff";
 import Button from "./components/Button";
 import { getHtmlDiff } from "./diff";
 
 const DiffViewer = ({ originalText, newText, onClose }) => {
+  const [raw, setRaw] = React.useState(false);
+
+  if (raw) {
+    return (
+      <div className="">
+        <Button onClick={onClose}>Close</Button>
+        <Button onClick={() => setRaw(false)}>Formatted</Button>
+        <div className="grid grid-cols-1 m-md font-mono">
+          {JsDiff.diffChars(originalText, newText).map((part, i) => {
+            return <pre key={i}>{JSON.stringify(part, null, 2)}</pre>;
+          })}
+        </div>
+      </div>
+    );
+  }
   const { originalLines, newLines } = getHtmlDiff(originalText, newText);
+
   return (
     <div className="">
       <Button onClick={onClose}>Close</Button>
+      <Button onClick={() => setRaw(true)}>Raw</Button>
       <div className="grid grid-cols-2 gap-4 m-md font-mono">
         <div className="p-sm bg-gray-100 dark:bg-gray-700 rounded-md">
           {originalLines}
