b779146522129a3be041fefda11192a8a201801b
Aditya Bhargava <adit@adit.io>
2023-05-01 11:11:35 -0500

    test for created_at timestamp

--- cypress/e2e/Editor.cy.ts
+++ cypress/e2e/Editor.cy.ts
@@ -17,9 +17,13 @@ describe("editor", () => {
 
     cy.selectChapter();
     cy.get("div[data-selector='text-editor-title']").type(`${title}{enter}`);
-    cy.get(".ql-editor").last().type(`${text}{enter}`);
 
-    
+    // Retest autosave twice. The first time, the created_at timestamp will be updated on the server
+    // and the new timestamp will be passed to the client. If it gets success fully updated,
+    // the second autosave will work. If it doesn't, the second autosave will fail.
+    cy.get(".ql-editor").last().type(`some `);
+    cy.autoSave();
+    cy.get(".ql-editor").last().type(`text {enter}`);
     cy.autoSave();
 
     cy.visit("http://localhost:80/");
