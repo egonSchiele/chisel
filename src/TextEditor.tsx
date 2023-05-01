1661cdb305a3202f38f1a4fc715ab6f9f28bf010
Aditya Bhargava <adit@adit.io>
2023-04-30 22:13:10 -0500

    reference badge

--- src/TextEditor.tsx
+++ src/TextEditor.tsx
@@ -233,8 +233,13 @@ function TextEditor({
                   }`}
                 />
               </div>
-              <div
-                className="h-5 cursor-pointer mr-xs mt-xs hidden"
+              {currentText.reference && (
+                <div className="m-0 p-0 mt-xs mr-xs text-center uppercase text-xs rounded border text-gray-500 border-gray-500">
+                  R
+                </div>
+              )}
+              {/* <div
+                className="h-5 cursor-pointer mr-xs mt-xs"
                 onClick={() => {
                   setOpen(false);
                 }}
@@ -244,7 +249,7 @@ function TextEditor({
                     isActive ? "text-gray-300" : "text-gray-500"
                   }`}
                 />
-              </div>
+              </div> */}
             </div>
 
             <div
