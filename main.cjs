d16b92ae1974088b92472d203ac5dada201c0ad5
Aditya Bhargava <adit@adit.io>
2023-05-14 13:16:34 -0500

    start

---
+++ main.cjs
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require("electron");
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("dist/login.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
