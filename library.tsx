bcf0654cc39efe0eefc32da1bb8c83e0120485bf
Aditya Bhargava <adit@adit.io>
2023-06-19 18:57:04 -0500

    remove styles when copying

--- library.tsx
+++ library.tsx
@@ -35,3 +35,11 @@ const registerServiceWorker = async () => {
 };
 
 registerServiceWorker();
+
+document.addEventListener("copy", function (e) {
+  const text_only = document.getSelection().toString();
+  const clipdata = e.clipboardData || window.clipboardData;
+  clipdata.setData("text/plain", text_only);
+  clipdata.setData("text/html", text_only);
+  e.preventDefault();
+});
