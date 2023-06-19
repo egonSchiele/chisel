9b70e8c7784fc15600d79b95a6fccf03412d0696
Aditya Bhargava <adit@adit.io>
2023-06-19 18:59:03 -0500

    ts-ignore

--- library.tsx
+++ library.tsx
@@ -38,6 +38,7 @@ registerServiceWorker();
 
 document.addEventListener("copy", function (e) {
   const text_only = document.getSelection().toString();
+  // @ts-ignore
   const clipdata = e.clipboardData || window.clipboardData;
   clipdata.setData("text/plain", text_only);
   clipdata.setData("text/html", text_only);
